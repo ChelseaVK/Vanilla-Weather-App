dd166e2d054a74a2b50b556c723a3b7ae7bd7885
ChelseaVK <cvanklaveren5@gmail.com>
2025-05-22 09:48:27 -0500

    Updated js.

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  return `${day} ${hours}:${minutes}`; 
+  return `${day} ${hours} : ${minutes}`; 
   if (minutes < 10) {
   minutes = `0${minutes}`};
+}
